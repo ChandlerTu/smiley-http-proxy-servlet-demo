b451585684ebf9ca74dcac795760ac9bcbd4dd5d
Chandler Tu <chandlertu@chandlertu.com>
2019-05-10 22:33:47 +0800

    增加 baidu 代理

---
+++ src/main/java/com/chandlertu/smiley/HomeController.java
@@ -0,0 +1,14 @@
+package com.chandlertu.smiley;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
+
+}
