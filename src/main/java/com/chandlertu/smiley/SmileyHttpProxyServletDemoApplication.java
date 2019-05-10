47cdb37198c527cf74468c81c1076bed8e29e10b
Chandler Tu <chandlertu@chandlertu.com>
2019-05-10 21:38:37 +0800

    创建 Spring Boot Web 项目

---
+++ src/main/java/com/chandlertu/smiley/SmileyHttpProxyServletDemoApplication.java
@@ -0,0 +1,13 @@
+package com.chandlertu.smiley;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmileyHttpProxyServletDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmileyHttpProxyServletDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chandlertu/smiley/SmileyHttpProxyServletDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.chandlertu.smiley;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SmileyHttpProxyServletDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
